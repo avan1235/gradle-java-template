fdb36129ec3e21d6ad2bcc377e28901ad67f2fd8
avan1235 <macieekprocyk@gmail.com>
2022-09-18 21:27:17 +0200

    config ant build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
     id("java")
+    kotlin("jvm") version "1.4.32"
 }
 
 group = "ml.dev.kotlin"
 version = "1.0"
 
 ant.importBuild("build.xml") { "ant-$it" }
+
+dependencies {
+    implementation(files("${System.getenv("NXJ_HOME")}/lib/nxt/classes.jar"))
+}

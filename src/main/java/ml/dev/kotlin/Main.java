7eff4fd1b13e6b6774cc866f9203ee87bdf7e227
avan1235 <macieekprocyk@gmail.com>
2022-09-18 22:11:04 +0200

    config ant build

--- src/main/java/ml/dev/kotlin/Main.java
+++
@@ -1,7 +0,0 @@
-package ml.dev.kotlin;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/ml/dev/kotlin/Main.kt
@@ -0,0 +1,8 @@
+package ml.dev.kotlin
+
+object Main {
+    @JvmStatic
+    fun main() {
+        println("fsdfsdf")
+    }
+}
